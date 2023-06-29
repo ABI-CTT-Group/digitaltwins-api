a69ce19d43874d9a2cfdd107ea880e321cc3f0d8
Chinchien <LIN810116@gmail.com>
2023-06-29 14:30:12 +1200

    Update download dataset example

--- examples/download_dataset.py
+++ examples/download_dataset.py
@@ -1,9 +1,10 @@
 from dtp.cores.downloader import Downloader
 
 if __name__ == '__main__':
-    config_file = "../configs/templates/pacs.json"
+    data_storage_config = "../configs/templates/pacs.json"
+    gen3_config = "../configs/templates/gen3.json"
     dataset_id = None
     dest = "./"
 
-    downloader = Downloader(config_file)
+    downloader = Downloader(data_storage_config=data_storage_config)
     downloader.download_dataset(dataset_id=dataset_id, dest=dest)
