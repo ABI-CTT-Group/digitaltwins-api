73d5ed4247b7e7286b96a11bcb15a5fb427cc433
Chinchien <LIN810116@gmail.com>
2023-07-12 11:10:03 +1200

    Rename convertor.py to metadata_convertor.py

--- dtp/__init__.py
+++ dtp/__init__.py
@@ -1,6 +1,6 @@
 from dtp.core.downloader import Downloader
 from dtp.gen3.auth import Auth
-from dtp.gen3.convertor import MetadataConvertor
+from dtp.gen3.metadata_convertor import MetadataConvertor
 from dtp.gen3.exporter import Exporter
 from dtp.gen3.querier import Querier
 from dtp.gen3.submitter import Gen3Submitter
