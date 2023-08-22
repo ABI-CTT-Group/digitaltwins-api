dcd60dfc00735691c4629b3972f3cd049f2ff660
Chinchien <LIN810116@gmail.com>
2023-08-22 17:27:51 +1200

    Remote extra comma

--- examples/gen3/convert_metadata.py
+++ examples/gen3/convert_metadata.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     sds_schema_dir = Path(r"/path/to/sds/dictionary/folder")
 
     convertor = MetadataConvertor(project=project, experiment=experiment, schema_dir=sds_schema_dir, version="2.0.0")
-    convertor.execute(source_dir=source_dir, dest_dir=dest_dir,)
+    convertor.execute(source_dir=source_dir, dest_dir=dest_dir)
