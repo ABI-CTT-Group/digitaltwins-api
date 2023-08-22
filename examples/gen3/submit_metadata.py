852f62d34e2f0dc6adc1ad35955ea567caa3d687
Chinchien <LIN810116@gmail.com>
2023-08-22 18:27:34 +1200

    Update example - submit metadata

--- examples/gen3/submit_metadata.py
+++ examples/gen3/submit_metadata.py
@@ -3,10 +3,12 @@ from pathlib import Path
 
 if __name__ == '__main__':
     endpoint = "GEN3_URL"
-    credentials = Path(r"/path/to/gen3/credentials/file")
+    cred_file = Path(r"/path/to/gen3/credentials/file")
     program = "demo1"
     project = "12L"
     file = Path(r"/path/to/gen3/json/metadata")
 
-    submitter = MetadataUploader(endpoint, str(credentials))
+    auth = Auth(endpoint, cred_file)
+
+    submitter = MetadataUploader(auth)
     submitter.submit_record(program, project, str(file))
