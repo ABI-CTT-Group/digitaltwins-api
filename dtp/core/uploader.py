245ecbd882c811923832c5b96fb1ff5e3bdbfcd3
Chinchien <LIN810116@gmail.com>
2023-08-22 18:29:27 +1200

    Add implementation of uploader.py

--- dtp/core/uploader.py
+++ dtp/core/uploader.py
@@ -20,6 +20,8 @@ class Uploader(object):
         :param gen3_config: (Optional)
         :type gen3_config: Path to the Gen3 configuration file (json)
         """
+        self._supported_sds_categories = ["dataset_description", "manifest", "subjects"]
+
         self._data_storage_configs = ConfigLoader.load_from_json(data_storage_config)
 
         self._data_storage = self._data_storage_configs.get("storage")
@@ -28,22 +30,37 @@ class Uploader(object):
         else:
             self._data_storage = self._data_storage_configs.get("storage")
 
+        if gen3_config:
+            self._gen3_config = ConfigLoader.load_from_json(gen3_config)
+            self._gen3_endpoint = self._gen3_config.get("gen3_endpoint")
+            self._gen3_cred_file = self._gen3_config.get("gen3_cred_file")
+            self._gen3_auth = Auth(self._gen3_endpoint, self._gen3_cred_file)
+            self._gen3_queryer = MetadataQuerier(self._gen3_auth)
+            self._gen3_uploader = MetadataUploader(self._gen3_endpoint, self._gen3_cred_file)
+
         if self._data_storage == "pacs":
             self._pacs_ip = self._data_storage_configs.get("pacs_ip")
             self._pacs_port = self._data_storage_configs.get("pacs_port")
             self._pacs_aec = self._data_storage_configs.get("pacs_aec")
             self._pacs_aet = self._data_storage_configs.get("pacs_aet")
 
-            self._gen3_config = ConfigLoader.load_from_json(gen3_config)
-            self._gen3_endpoint = self._gen3_config.get("gen3_endpoint")
-            self._gen3_cred_file = self._gen3_config.get("gen3_cred_file")
-            self._gen3_auth = Auth(self._gen3_endpoint, self._gen3_cred_file)
-            self._gen3_queryer = MetadataQuerier(self._gen3_auth)
-
         elif self._data_storage == "irods":
             self._irods = IRODS(data_storage_config)
 
-    def upload(self):
+    def upload(self, dataset_dir, project):
+        dataset_dir = Path(dataset_dir)
+        self._verify_dataset(dataset_dir, project)
+
+        self.upload_metadata(dataset_dir, project)
+        self.upload_files(dataset_dir)
+
+    def upload_metadata(self, dataset_dir, project):
+        # convert sds metadata to gen3
+        convertor = MetadataConvertor(project=project, experiment=dataset_dir.name)
+        convertor.execute(source_dir=dataset_dir, dest_dir="./")
+
+        # upload metadata
+        MetadataUploader
         pass
 
     def upload_metadata(self):
