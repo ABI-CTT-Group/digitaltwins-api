b6139dae7d89ba2b14bafb6ced2db36b4ea04cd3
Chinchien Lin <lin810116@gmail.com>
2023-09-14 10:50:39 +1200

    Bump up version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="digitaltwins",
-    version="0.1.0",
+    version="0.1.1",
     description='A Python tool for interacting with the 12 Labours DigitalTWINS (Digital Translational Workflows for '
                 'Integrating Systems) Platform',
     # long_description=long_description,
