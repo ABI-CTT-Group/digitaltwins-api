4462048b7a8d61627cc2ed3ea6af221aaa582dcb
Chinchien Lin <lin810116@gmail.com>
2023-09-15 10:43:38 +1200

    Add Deleter class skeleton

---
+++ digitaltwins/core/deleter.py
@@ -0,0 +1,25 @@
+import configparser
+from pathlib import Path
+
+
+class Deleter(object):
+    def __init__(self, config_file):
+        self._configs = configparser.ConfigParser()
+        self._configs.read(config_file)
+
+        self._program = self._configs["gen3"].get("program")
+        self._project = self._configs["gen3"].get("project")
+
+        self._gen3_endpoint = self._configs["gen3"].get("endpoint")
+        self._gen3_cred_file = Path(self._configs["gen3"].get("cred_file"))
+
+    def execute(self, dataset_name):
+        self._delete_metadata(dataset_name)
+        self._delete_dataset(dataset_name)
+        pass
+
+    def _delete_metadata(self, dataset_name):
+        pass
+
+    def _delete_dataset(self, dataset_name):
+        pass
