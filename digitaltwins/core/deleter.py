0620e941cade16d9b2e7795306c28351a72a1120
Chinchien Lin <lin810116@gmail.com>
2023-09-15 11:50:25 +1200

    Change class var visibility

--- digitaltwins/core/deleter.py
+++ digitaltwins/core/deleter.py
@@ -1,10 +1,12 @@
 import configparser
 from pathlib import Path
 
+from digitaltwins import MetadataQuerier
+
 
 class Deleter(object):
     def __init__(self, config_file):
-        self.config_file = Path(config_file)
+        self._config_file = Path(config_file)
 
         self._configs = configparser.ConfigParser()
         self._configs.read(str(config_file))
@@ -15,13 +17,15 @@ class Deleter(object):
         self._gen3_endpoint = self._configs["gen3"].get("endpoint")
         self._gen3_cred_file = Path(self._configs["gen3"].get("cred_file"))
 
-    def execute(self, dataset_name):
-        self._delete_metadata(dataset_name)
-        self._delete_dataset(dataset_name)
+    def execute(self, dataset_id):
+        self._delete_metadata(dataset_id)
+        self._delete_dataset(dataset_id)
         pass
 
-    def _delete_metadata(self, dataset_name):
+    def _delete_metadata(self, dataset_id):
+        querier = MetadataQuerier(self._config_)
+        records = querier.get_dataset_records(dataset_id=datasset_id)
         pass
 
-    def _delete_dataset(self, dataset_name):
+    def _delete_dataset(self, dataset_id):
         pass
