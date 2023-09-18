7ad85b1ee02c5f90b40f210a8c815aa11547630a
Chinchien Lin <lin810116@gmail.com>
2023-09-18 14:25:37 +1200

    Update examples

--- examples/query_metadata.py
+++ examples/query_metadata.py
@@ -3,32 +3,38 @@ from pathlib import Path
 import configparser
 
 if __name__ == '__main__':
-    config_file = Path(r"/path/to/configs_ctt.ini")
+    config_file = Path(r"/path/to/configs.ini")
 
     querier = MetadataQuerier(config_file)
 
-    # # List programs
+    # List programs
     programs = querier.get_programs_all()
     print("Programs: " + str(programs))
-    #
-    # # List projects
-    configs = configparser.ConfigParser()
-    configs.read(config_file)
-    projects = querier.get_projects_by_program(program=configs["gen3"].get("program"))
+
+    # List projects
+    program = "PROGRAM_NAME"
+    projects = querier.get_projects_by_program(program=program)
     print("projects: " + str(projects))
 
     # List datasets
     datasets = querier.get_datasets()
-    print("Datasets: " + str(datasets))
+    for dataset in datasets:
+        print("Dataset: " + dataset.get_id())
 
-    # # List subjects
-    dataset_id = ""
-    subjects = querier.get_subjects(dataset_id=dataset_id)
-    print("Subjects: " + str(subjects))
+    # Get dataset by id
+    dataset_id = "DATASET_ID"
+    dataset = querier.get_dataset(dataset_id)
+    if dataset:
+        print("Dataset: " + dataset.get_id())
+    else:
+        raise ValueError("Dataset not found")
 
-    # Get dataset descriptions
-    dataset_id = ""
-    dataset_descriptions = querier.get_dataset_descriptions(dataset_id=dataset_id)
-    print("Dataset description: " + str(dataset_descriptions))
+    # get dataset_description
+    metadata = dataset.get_metadata(metadata="dataset_description")
+    print("dataset_description: " + str(metadata))
+
+    # get subjects
+    metadata = dataset.get_metadata(metadata="subjects")
+    print("subjects: " + str(metadata))
 
     print("done")
--- examples/upload_dataset.py
+++ examples/upload_dataset.py
@@ -4,7 +4,7 @@ from pathlib import Path
 if __name__ == '__main__':
     dataset_dir = Path(r"/path/to/dataset_dir")
 
-    uploader = Uploader(Path(r"/path/to/configs_ctt.ini"))
+    uploader = Uploader(Path(r"/path/to/configs.ini"))
 
     uploader.execute(dataset_dir=dataset_dir)
     print("done")
