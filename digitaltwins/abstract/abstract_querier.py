1d68c7d2427c08e320b3d372754a26de2b9b4833
Chinchien <LIN810116@gmail.com>
2025-02-12 14:50:17 +1300

    feat: simplify abstract_querier.py

--- digitaltwins/abstract/abstract_querier.py
+++ digitaltwins/abstract/abstract_querier.py
@@ -22,23 +22,3 @@ class AbstractQuerier(ABC):
 
         self._MAX_ATTEMPTS = 10
 
-    @abstractmethod
-    def get_programs(self):
-        pass
-
-    @abstractmethod
-    def get_projects(self):
-        pass
-
-    @abstractmethod
-    def get_datasets(self, descriptions=False, categories=list(), keywords=dict()):
-        pass
-
-    @abstractmethod
-    def get_subjects(self, dataset_uuid):
-        pass
-
-    @abstractmethod
-    def get_samples(self, dataset_uuid=None, subject_uuid=None):
-        pass
-
