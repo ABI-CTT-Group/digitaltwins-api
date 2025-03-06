740c9e41afa9ab488b65dec2a5198c6ba8dae798
Chinchien <LIN810116@gmail.com>
2025-03-06 15:49:58 +1300

    feat: download assay input data

--- digitaltwins/core/downloader.py
+++ digitaltwins/core/downloader.py
@@ -1,7 +1,10 @@
 import configparser
 import os
+import shutil
 
 from ..irods.irods import IRODS
+
+from digitaltwins import Querier
 
 
 class Downloader(object):
@@ -26,3 +29,42 @@ class Downloader(object):
             print("Dataset successfully downloaded")
         else:
             raise EnvironmentError("Missing Downloader. Please check your configurations for data storage.")
+
+    def download_assay_inputs(self, assay_id, save_dir="./tmp"):
+        querier = Querier(self._config_file)
+        assay = querier.get_assay(assay_id, get_params=True)
+
+        params = assay.get("params")
+        assay_uuid = params.get("assay_uuid")
+        inputs = params.get("inputs")
+
+        for input in inputs:
+            name = input.get("name")
+            save_dir_input = os.path.join(save_dir, assay_uuid, "input", name)
+            os.makedirs(save_dir_input)
+
+            dataset_uuid = input.get("dataset_uuid")
+            sample_type = input.get("sample_type")
+            category = input.get("category")
+            if category == "measurement":
+                # get sample uuids by sample_type
+                samples = querier.get_dataset_samples(dataset_uuid=dataset_uuid, sample_type=sample_type)
+                print(samples)
+                for sample in samples:
+                    subject_id = sample.get("subject_id")
+                    sample_id = sample.get("sample_id")
+                    sample_uuid = sample.get("sample_uuid")
+
+                    sample_path = dataset_uuid + "/primary/" + subject_id + "/" + sample_id
+
+                    local_sample_path_tmp = save_dir_input + "/" + sample_id
+                    if os.path.exists(local_sample_path_tmp):
+                        shutil.rmtree(local_sample_path_tmp)
+
+                    self._irods_downloader.download(sample_path, save_dir_input)
+
+                    local_sample_path = save_dir_input + "/" + sample_uuid
+                    if os.path.exists(local_sample_path):
+                        shutil.rmtree(local_sample_path)
+
+                    os.rename(local_sample_path_tmp, local_sample_path)
