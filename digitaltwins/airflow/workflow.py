96cc328c721e57aa7be270253b46b174dba88f68
Chinchien <LIN810116@gmail.com>
2025-03-12 15:24:11 +1300

    feat: trigger preprocessor workflow from workflow.run()

--- digitaltwins/airflow/workflow.py
+++ digitaltwins/airflow/workflow.py
@@ -24,8 +24,10 @@ class Workflow(object):
     def run(self, assay):
         assay_seek_id = assay.get("id")
 
+        assay_params = assay.get("params")
+
         # get dag_url
-        workflow_seek_id = assay.get("params").get('workflow_seek_id')
+        workflow_seek_id = assay_params.get('workflow_seek_id')
         querier = Querier(self._config_file)
         workflow = querier.get_sop(sop_id=workflow_seek_id)
         workflow_dataset_uuid = workflow.get("dataset_uuid")
@@ -33,18 +35,29 @@ class Workflow(object):
         # dag_rul_ui = f"http://0.0.0.0:8080/dags/{workflow_dataset_uuid}/grid"
         dag_rul_ui = f"{self._airflow_endpoint}/dags/{workflow_dataset_uuid}/grid"
 
+        # subject_uuid = None
+
         params = {
+            # "subject_uuid": subject_uuid,
             "assay_seek_id": assay_seek_id,
             "workspace": None,
-            "platform_config_file": None,
+            # "platform_configs": None,
         }
 
+        preprocessor_dag_url = f"{self._airflow_api_url}/dags/preprocessor/dagRuns"
         response = requests.post(
-            dag_url,
+            preprocessor_dag_url,
             auth=HTTPBasicAuth(self._username, self._password),
             headers={"Content-Type": "application/json"},
             data=json.dumps({"conf": params})
         )
+
+        # response = requests.post(
+        #     dag_url,
+        #     auth=HTTPBasicAuth(self._username, self._password),
+        #     headers={"Content-Type": "application/json"},
+        #     data=json.dumps({"conf": params})
+        # )
 
         return response, dag_rul_ui
 
