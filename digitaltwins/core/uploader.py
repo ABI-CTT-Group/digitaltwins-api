99ce7033dd019de56b12765c8bd1d3aea7ff99fa
Chinchien <LIN810116@gmail.com>
2025-06-09 14:30:14 +1200

    fix: typo

--- digitaltwins/core/uploader.py
+++ digitaltwins/core/uploader.py
@@ -15,14 +15,14 @@ class Uploader(object):
         if self._configs.getboolean("postgres", "enabled") and self._configs.getboolean("gen3", "enabled"):
             raise ValueError("Metadata service conflict. Only one of 'postgres' or 'gen3' can be enabled")
 
-        self._postgre_uploader = None
+        self._postgres_uploader = None
         self._gen3_uploader = None
         self._irods_uploader = None
 
         if self._configs.getboolean("postgres", "enabled"):
-            self._postgre_uploader = PostgresUploader(config_file)
+            self._postgres_uploader = PostgresUploader(config_file)
         else:
-            self._postgre_uploader = None
+            self._postgres_uploader = None
 
         # if self._configs.getboolean("gen3", "enabled"):
         #     self._gen3_uploader = Gen3Uploader(config_file)
@@ -35,5 +35,5 @@ class Uploader(object):
 
 
     def upload_assay(self, assay_data):
-        self._postgre_uploader.upload_assay(assay_data)
+        self._postgres_uploader.upload_assay(assay_data)
 
