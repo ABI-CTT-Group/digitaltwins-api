0910b128b4b825066579624e30809bb4a70225f0
Chinchien <LIN810116@gmail.com>
2026-03-10 11:27:35 +1300

    docs: add docstrings to rest api
    Google style

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,9 @@
 """
 IAM by Keycloak
+
+This module handles identity and access management using Keycloak. It provides functions
+and endpoints for user authentication, token retrieval, and token validation using both
+Basic and Bearer authentication schemes.
 """
 import requests
 import os
@@ -27,6 +31,18 @@ keycloak_introspect_url = f"{keycloak_token_url}/introspect"
 
 
 def auth_basic(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
+    """
+    Authenticate a user using Basic Authentication.
+
+    Args:
+        credentials (HTTPBasicCredentials, optional): The base64 encoded username and password.
+
+    Returns:
+        bool: True if authentication is successful.
+
+    Raises:
+        HTTPException: If the username or password is invalid (Status 401).
+    """
     result = get_token(credentials)
     if not result.get("access_token"):
         raise HTTPException(
@@ -38,6 +54,20 @@ def auth_basic(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
 
 
 def auth_bearer(credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    """
+    Authenticate a user using Bearer Token Authentication.
+
+    Validates the bearer token by introspecting it against Keycloak.
+
+    Args:
+        credentials (HTTPAuthorizationCredentials, optional): The bearer token provided in the Authorization header.
+
+    Returns:
+        bool: True if the token is active and valid.
+
+    Raises:
+        HTTPException: If the token is invalid or inactive (Status 401).
+    """
     token = credentials.credentials
     data = {
         "token": token,
@@ -60,6 +90,19 @@ def validate_credentials(
         basic_credentials: HTTPBasicCredentials = Depends(HTTPBasic(auto_error=False)),
         bearer_credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False))
 ):
+    """
+    Validate incoming credentials, allowing either Basic or Bearer schemes.
+
+    Args:
+        basic_credentials (HTTPBasicCredentials, optional): The Basic auth credentials.
+        bearer_credentials (HTTPAuthorizationCredentials, optional): The Bearer auth credentials.
+
+    Returns:
+        bool: True if either of the provided credentials is valid.
+
+    Raises:
+        HTTPException: If no valid authentication method is provided (Status 401).
+    """
     if basic_credentials:
         valid = auth_basic(basic_credentials)
         return valid
@@ -76,6 +119,15 @@ def validate_credentials(
 
 @router.post("/login", tags=["auth"])
 def login(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
+    """
+    Log in a user to retrieve an access token.
+
+    Args:
+        credentials (HTTPBasicCredentials, optional): Basic auth containing username and password.
+
+    Returns:
+        dict: The Keycloak token response, usually containing an access_token.
+    """
     # validate_credentials(credentials)
     result = get_token(basic_credentials=credentials)
     return result
@@ -86,6 +138,19 @@ def get_token(
         basic_credentials: HTTPBasicCredentials = Depends(HTTPBasic(auto_error=False)),
         bearer_credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False))
 ):
+    """
+    Retrieve or refresh an access token from Keycloak.
+
+    Args:
+        basic_credentials (HTTPBasicCredentials, optional): Provided to retrieve a token via password grant.
+        bearer_credentials (HTTPAuthorizationCredentials, optional): Provided to refresh a token via refresh_token grant.
+
+    Returns:
+        dict: The JSON representation of the token response.
+
+    Raises:
+        HTTPException: If no valid authentication method is provided or if credential validation fails (Status 401).
+    """
     if basic_credentials:
         payload = {
             "client_id": KEYCLOAK_CLIENT_ID,
@@ -121,4 +186,13 @@ def get_token(
 
 @router.get("/verify_token", tags=["auth"])
 def verify_token(credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    """
+    Verify the validity of a Bearer token.
+
+    Args:
+        credentials (HTTPAuthorizationCredentials, optional): Bearer token from the Authorization header.
+
+    Returns:
+        dict: A dictionary containing an 'active' key that is True if the token is valid.
+    """
     return {"active": auth_bearer(credentials)}
--- app/routers/query.py
+++ app/routers/query.py
@@ -1,3 +1,9 @@
+"""
+Query Routings.
+
+This module provides various API endpoints for querying digital twins resources, such as
+programs, projects, investigations, studies, assays, workflows, and tools via the Querier.
+"""
 import os
 
 from dotenv import load_dotenv
@@ -14,81 +20,220 @@ router = APIRouter()
 
 @router.get("/programs", tags=["query"])
 def get_programs(get_details=False, valid=Depends(validate_credentials)):
+    """
+    Retrieve a list of programs.
+
+    Args:
+        get_details (bool, optional): If True, returns detailed information about each program. Defaults to False.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the list of programs under the 'programs' key.
+    """
     programs = querier.get_programs(get_details=get_details)
     return {"programs": programs}
 
 
 @router.get("/programs/{program_id}", tags=["query"])
 def get_program(program_id=None, valid=Depends(validate_credentials)):
+    """
+    Retrieve a specific program by its ID.
+
+    Args:
+        program_id (str, optional): The ID of the program to retrieve.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the program details under the 'program' key.
+    """
     program = querier.get_program(program_id)
     return {"program": program}
 
 
 @router.get("/projects", tags=["query"])
 def get_projects(get_details=False, valid=Depends(validate_credentials)):
+    """
+    Retrieve a list of projects.
+
+    Args:
+        get_details (bool, optional): If True, returns detailed information about each project. Defaults to False.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the list of projects under the 'projects' key.
+    """
     projects = querier.get_projects(get_details=get_details)
     return {"projects": projects}
 
 
 @router.get("/projects/{project_id}", tags=["query"])
 def get_project(project_id=None, valid=Depends(validate_credentials)):
+    """
+    Retrieve a specific project by its ID.
+
+    Args:
+        project_id (str, optional): The ID of the project to retrieve.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the project details under the 'project' key.
+    """
     project = querier.get_project(project_id)
     return {"project": project}
 
 
 @router.get("/investigations", tags=["query"])
 def get_investigations(get_details=False, valid=Depends(validate_credentials)):
+    """
+    Retrieve a list of investigations.
+
+    Args:
+        get_details (bool, optional): If True, returns detailed information about each investigation. Defaults to False.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the list of investigations under the 'investigations' key.
+    """
     investigations = querier.get_investigations(get_details=get_details)
     return {"investigations": investigations}
 
 
 @router.get("/investigations/{investigation_id}", tags=["query"])
 def get_investigation(investigation_id=None, valid=Depends(validate_credentials)):
+    """
+    Retrieve a specific investigation by its ID.
+
+    Args:
+        investigation_id (str, optional): The ID of the investigation to retrieve.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the investigation details under the 'investigation' key.
+    """
     investigation = querier.get_investigation(investigation_id)
     return {"investigation": investigation}
 
 
 @router.get("/studies", tags=["query"])
 def get_studies(get_details=False, valid=Depends(validate_credentials)):
+    """
+    Retrieve a list of studies.
+
+    Args:
+        get_details (bool, optional): If True, returns detailed information about each study. Defaults to False.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the list of studies under the 'studies' key.
+    """
     studies = querier.get_studies(get_details=get_details)
     return {"studies": studies}
 
 
 @router.get("/studies/{study_id}", tags=["query"])
 def get_study(study_id=None, valid=Depends(validate_credentials)):
+    """
+    Retrieve a specific study by its ID.
+
+    Args:
+        study_id (str, optional): The ID of the study to retrieve.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the study details under the 'study' key.
+    """
     study = querier.get_study(study_id)
     return {"study": study}
 
 
 @router.get("/assays", tags=["query"])
 def get_assays(get_details=False, valid=Depends(validate_credentials)):
+    """
+    Retrieve a list of assays.
+
+    Args:
+        get_details (bool, optional): If True, returns detailed information about each assay. Defaults to False.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the list of assays under the 'assays' key.
+    """
     assays = querier.get_assays(get_details=get_details)
     return {"assays": assays}
 
 
 @router.get("/assays/{assay_id}", tags=["query"])
 def get_assay(assay_id=None, get_params=False, valid=Depends(validate_credentials)):
+    """
+    Retrieve a specific assay by its ID, with optional parameters.
+
+    Args:
+        assay_id (str, optional): The ID of the assay to retrieve.
+        get_params (bool, optional): If True, retrieves additional parameters related to the assay. Defaults to False.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the assay details under the 'assay' key.
+    """
     assay = querier.get_assay(assay_id, get_params=get_params)
     return {"assay": assay}
 
 @router.get("/workflows", tags=["query"])
 def get_workflows(valid=Depends(validate_credentials)):
+    """
+    Retrieve a list of workflows.
+
+    Args:
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the list of workflows under the 'workflows' key.
+    """
     workflows = querier.get_workflows()
     return {"workflows": workflows}
 
 
 @router.get("/workflows/{workflow_id}", tags=["query"])
-def get_workflow(workflow_id=None, valid=Depends(validate_credentials)):
+# def get_workflow(workflow_id=None, valid=Depends(validate_credentials)):
+def get_workflow(workflow_id=None):
+    """
+    Retrieve a specific workflow by its ID.
+
+    Args:
+        workflow_id (str, optional): The ID of the workflow to retrieve.
+
+    Returns:
+        dict: A dictionary containing the workflow details under the 'workflow' key.
+    """
     workflow = querier.get_workflow(workflow_id)
     return {"workflow": workflow}
 
 @router.get("/tools", tags=["query"])
 def get_tools(valid=Depends(validate_credentials)):
+    """
+    Retrieve a list of tools.
+
+    Args:
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the list of tools under the 'tools' key.
+    """
     tools = querier.get_tools()
     return {"tools": tools}
 
 
 @router.get("/tools/{tool_id}", tags=["query"])
 def get_tool(tool_id=None, valid=Depends(validate_credentials)):
+    """
+    Retrieve a specific tool by its ID.
+
+    Args:
+        tool_id (str, optional): The ID of the tool to retrieve.
+        valid (bool): Ensures valid credentials are provided.
+
+    Returns:
+        dict: A dictionary containing the tool details under the 'tool' key.
+    """
     tool = querier.get_tool(tool_id)
     return {"tool": tool}
