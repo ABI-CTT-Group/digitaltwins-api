89bf7a2aae8a6104990cc8adc25ef59b85733965
Chinchien <LIN810116@gmail.com>
2026-03-12 16:33:12 +1300

    refactor: rename var

--- src/digitaltwins/core/uploader.py
+++ src/digitaltwins/core/uploader.py
@@ -11,31 +11,32 @@ from ..irods.uploader import Uploader as IRODSUploader
 
 class Uploader(object):
     def __init__(self):
-        postgres_enabled = is_truthy(os.getenv("POSTGRES_ENABLED"))
-        gen3_enabled = is_truthy(os.getenv("GEN3_ENABLED"))
-        irods_enabled = is_truthy(os.getenv("IRODS_ENABLED"))
+        self._postgres_enabled = is_truthy(os.getenv("POSTGRES_ENABLED"))
+        self._gen3_enabled = is_truthy(os.getenv("GEN3_ENABLED"))
+        self._irods_enabled = is_truthy(os.getenv("IRODS_ENABLED"))
 
-        if postgres_enabled and gen3_enabled:
+        if self._postgres_enabled and self._gen3_enabled:
             raise ValueError("Metadata service conflict. Only one of 'postgres' or 'gen3' can be enabled")
 
         self._postgres_uploader = None
         self._gen3_uploader = None
         self._irods_uploader = None
 
-        if postgres_enabled:
+        if self._postgres_enabled:
             self._postgres_uploader = PostgresUploader()
         else:
             self._postgres_uploader = None
 
-        # if gen3_enabled:
+        # if self._gen3_enabled:
         #     self._gen3_uploader = Gen3Uploader()
         #
 
-        if irods_enabled:
+        if self._irods_enabled:
             self._irods_uploader = IRODSUploader()
         else:
             self._irods_uploader = None
 
 
     def upload_assay(self, assay_data):
-        self._postgres_uploader.upload_assay(assay_data)
+        if self._postgres_enabled:
+            self._postgres_uploader.upload_assay(assay_data)
