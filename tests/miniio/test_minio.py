5c1bff4ca1c2f176b4f69f957597a3d076820110
chinchien-lin <clin864@aucklanduni.ac.nz>
2026-03-30 15:35:03 +1300

    chore: move test data

--- tests/miniio/test_minio.py
+++ tests/miniio/test_minio.py
@@ -3,14 +3,15 @@ from digitaltwins.minio.uploader import Uploader
 
 if __name__ == "__main__":
     bucket_name = "measurements"
-    test_file = Path("./tests/miniio/data/test.txt")
+    script_dir = Path(__file__).resolve().parent
 
     uploader = Uploader()
     uploader.bucket_exists(bucket_name)
 
     # Upload a file
+    test_file = script_dir / "data/test.txt"
     uploader.upload_file(str(test_file), bucket_name, test_file.name, overwrite=False)
-    
+
     # Upload a folder
-    test_folder = Path("./tests/miniio/data")
-    uploader.upload_folder(str(test_folder), bucket_name, "test_folder", overwrite=False)
+    test_folder = script_dir / "data/test_folder"
+    uploader.upload_folder(str(test_folder), bucket_name, test_folder.name, overwrite=False)
