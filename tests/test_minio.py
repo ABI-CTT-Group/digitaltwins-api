a878052b4224d8b5820993c0b4dcb6d7a4e64a46
chinchien-lin <clin864@aucklanduni.ac.nz>
2026-03-31 15:34:49 +1300

    test [minio]: delete bucket

--- tests/test_minio.py
+++ tests/test_minio.py
@@ -1,17 +1,32 @@
 from pathlib import Path
 from digitaltwins.minio.uploader import Uploader
+from digitaltwins.minio.deleter import Deleter
 
-if __name__ == "__main__":
+SCRIPT_DIR = Path(__file__).resolve().parent
+
+def test_upload():
     bucket_name = "measurements"
-    script_dir = Path(__file__).resolve().parent
 
     uploader = Uploader()
     uploader.bucket_exists(bucket_name)
 
     # Upload a file
-    test_file = script_dir / "data/test.txt"
+    test_file = SCRIPT_DIR / "data/test.txt"
     uploader.upload_file(str(test_file), bucket_name, test_file.name, overwrite=False)
 
     # Upload a folder
-    test_folder = script_dir / "data/test_folder"
+    test_folder = SCRIPT_DIR / "data/test_folder"
     uploader.upload_folder(str(test_folder), bucket_name, test_folder.name, overwrite=False)
+
+
+def test_delete_bucket():
+    bucket_name = "test"
+
+    deleter = Deleter()
+    resp = deleter.delete_bucket(bucket_name)
+    print(resp)
+
+
+if __name__ == "__main__":
+    # test_upload()
+    test_delete_bucket()
