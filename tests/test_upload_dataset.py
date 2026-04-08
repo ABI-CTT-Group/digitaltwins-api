bd987a8c87377b0b582e64cc8bda85b2c31ee6ae
chinchien-lin <clin864@aucklanduni.ac.nz>
2026-04-08 16:57:57 +1200

    fix [test]: correct path

--- tests/test_upload_dataset.py
+++ tests/test_upload_dataset.py
@@ -3,7 +3,7 @@ from digitaltwins.core.uploader import Uploader
 
 if __name__ == "__main__":
     script_dir = Path(__file__).resolve().parent
-    datasets_dir = script_dir / "../resources/example_sds_dataset"
+    datasets_dir = script_dir / "data/example_sds_dataset"
 
     uploader = Uploader()
     dataset_uuid = uploader.upload_dataset(
